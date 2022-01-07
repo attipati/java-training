d27203df12319d031170faf44da98a040e9320dc
attipati <96856522+attipati@users.noreply.github.com>
2022-01-07 18:14:41 +0530

    Add files via upload

---
+++ Egg.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Egg {
+        public static void main(String[] args) {
+            System.out.println("  _______");
+            System.out.println(" /       \\");
+            System.out.println("/         \\");
+            System.out.println("-\"-'-\"-'-\"-");
+            System.out.println("\\         /");
+            System.out.println(" \\_______/");
+        }
+    }
+    
+
